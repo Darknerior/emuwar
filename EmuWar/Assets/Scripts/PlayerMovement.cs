d7d89769e7a4719444896911471d04d40d0494fd
Darknerior <92634359+Darknerior@users.noreply.github.com>
2024-02-07 17:47:59 +0000

    Added jumping, as well as "accurate" emu movement

--- EmuWar/Assets/Scripts/PlayerMovement.cs
+++ EmuWar/Assets/Scripts/PlayerMovement.cs
@@ -1,55 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
+    //Controller Local variables
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private Transform playerTransform;
-    [SerializeField]float playerSpeed = 2.0f;
-    [SerializeField]float topPlayerSpeed = 13.85184f;//TOP EMU SPEED 31MPH
-    [SerializeField]float playerRotationSpeed = 2.0f;
-    [SerializeField]private float jumpHeight = 1.0f;
-    [SerializeField] private float gravityValue = -9.81f;
-
-    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    private float defaultPlayerSpeed;
+    
+    //Serialized Variables
+    [SerializeField]private float playerSpeed =  1.12f;//EMU WALK SPEED 2.5MPH
+    [SerializeField]private float maxPlayerSpeed = 13.85f;//TOP EMU SPEED 31MPH
+    [SerializeField]private float playerAcceleration = 4.47f;//CHATGPT ESTIMATE
+    [SerializeField]private float playerRotationSpeed = 2.0f;
+    [SerializeField]private float jumpHeight = 2.1f;//EMU JUMP HEIGHT
+    [SerializeField]private float gravityValue = -9.81f;
+    [SerializeField]private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField]private KeyCode sprintKey = KeyCode.LeftShift;
+    
     
     private void Start() {
         controller = GetComponent<CharacterController>();
         playerTransform = transform;
+        defaultPlayerSpeed = playerSpeed;
     }
     
     private void Update() {
         Move();
         Rotate();
     }
+    
+    
+    /// <summary>
+    /// Movement for the controller
+    /// </summary>
+    private void Move() {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)playerVelocity.y = 0f;
+        
 
-    private void Jump()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private void Move()
-    {
         // Calculate movement direction based on input
         var moveDirection = Vector3.zero;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetAxisRaw("Vertical") > 0) {
             moveDirection = playerTransform.forward;
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            if(playerSpeed < maxPlayerSpeed && Input.GetKey(sprintKey))playerSpeed += playerAcceleration * Time.deltaTime;//Accelerate
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0) {
             moveDirection = -playerTransform.forward;
+        }
+        
+        //Reset Player Speed    
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(sprintKey)) playerSpeed = defaultPlayerSpeed;
+        
 
         // Move the player using CharacterController
-        controller.Move(moveDirection * playerSpeed * Time.deltaTime);
+        controller.Move((playerSpeed * moveDirection + playerVelocity) * Time.deltaTime);
         
-        //ADD ACCELETATION
+        // Changes the height position of the player..
+        var canJump = playerSpeed <= defaultPlayerSpeed;
+        if (Input.GetKeyDown(jumpKey) && groundedPlayer && canJump)playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
 
     }
-
-    private void Rotate()
-    {
+    
+    /// <summary>
+    /// Rotation for the controller
+    /// </summary>
+    private void Rotate() {
+        //Rotates based on horizontal input
         transform.Rotate(new Vector3(0,Input.GetAxisRaw("Horizontal") * playerRotationSpeed, 0));
     }
 
