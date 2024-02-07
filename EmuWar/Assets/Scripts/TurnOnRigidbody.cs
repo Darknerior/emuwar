d326e70c5d375f7c510c8e50f09bb3b170bafb86
Darknerior <92634359+Darknerior@users.noreply.github.com>
2024-02-07 18:41:43 +0000

    Added destructible rabbit fences

---
+++ EmuWar/Assets/Scripts/TurnOnRigidbody.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TurnOnRigidbody : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void Enable()
+    {
+        rb.useGravity = true;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.freezeRotation = false;
+    }
+}
